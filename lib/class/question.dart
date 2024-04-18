ca0e9ee09c17a26db0c81ee666a0ddda9de0d4b5
Andreas Bayu Prakasa <abayup1498@gmail.com>
2024-04-18 15:13:42 +0700

    Update Gambar udah bisa ganti2

---
+++ lib/class/question.dart
@@ -0,0 +1,13 @@
+class Question {
+  String photo_a;
+  String photo_b;
+  String photo_c;
+  String photo_d;
+
+  Question(
+    this.photo_a,
+    this.photo_b,
+    this.photo_c,
+    this.photo_d,
+  );
+}
--- lib/screen/game.dart
+++ lib/screen/game.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -9,11 +10,39 @@ class Game extends StatefulWidget {
   }
 }
 
+generateRandomPicture(int min, int max) {
+  Random ran = Random();
+  Set<int> numPict = Set();
+
+  while (numPict.length < 5) {
+    int angka = min + ran.nextInt(max - min + 1);
+    numPict.add(angka);
+  }
+  return numPict.toList();
+}
+
+generateRandomAnswer(int min, int max) {
+  Random ran = Random();
+  List<int> numAns = List.empty(growable: true);
+
+  while (numAns.length < 5) {
+    int angka = min + ran.nextInt(max - min + 1);
+    numAns.add(angka);
+  }
+  return numAns;
+}
+
 class _GameState extends State<Game> {
   late Timer _timer;
   int _waktuIngat = 3;
   int _waktuJawab = 30;
   int _detik = 0;
+
+  int _picture_no = 0;
+  String _picture_img = "";
+  List<int> _picture = generateRandomPicture(1, 20);
+  List<int> _answer = generateRandomAnswer(1, 4);
+  List<int> _list_num = [1, 2, 3, 4];
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +50,17 @@ class _GameState extends State<Game> {
       appBar: AppBar(
         title: const Text("MemorImage"),
       ),
-      body: Column(
-        children: <Widget>[],
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              formatTime(_detik),
+              style: const TextStyle(fontSize: 20),
+            ),
+            Text("Memorize this"),
+            Image.asset("assets/images/${_picture_img}.png")
+          ],
+        ),
       ),
     );
   }
@@ -32,6 +70,9 @@ class _GameState extends State<Game> {
     super.initState();
     _detik = _waktuIngat;
     startTimer();
+    print(_picture.toString());
+    print(_answer.toString());
+    _picture_img = "c-${_picture[0]}-${_answer[0]}";
   }
 
   String formatTime(int detik) {
@@ -45,6 +86,14 @@ class _GameState extends State<Game> {
     _timer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {
       setState(() {
         _detik--;
+        if (_detik == 0) {
+          _picture_no++;
+          _detik = _waktuIngat;
+          _picture_img = "c-${_picture[_picture_no]}-${_answer[_picture_no]}";
+          if (_picture_no > _picture.length - 1) {
+            _picture_no = 0;
+          }
+        }
       });
     });
   }
