e1c554883d465c669441093a0f91562f3f49e69a
Andreas Bayu Prakasa <abayup1498@gmail.com>
2024-04-18 13:50:03 +0700

    Update File Game Result

---
+++ lib/screen/game.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Game extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _GameState();
+  }
+}
+
+class _GameState extends State<Game> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+  
+}
