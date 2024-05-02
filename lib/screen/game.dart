35421b88417715df9489f904ee22496177e4976e
Andreas Bayu Prakasa <abayup1498@gmail.com>
2024-05-02 12:28:21 +0700

    Update game.dart

--- lib/screen/game.dart
+++ lib/screen/game.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class Game extends StatefulWidget {
   @override
@@ -11,43 +12,42 @@ class Game extends StatefulWidget {
 
 class _GameState extends State<Game> {
   late Timer _timer;
-  int _memorizationDuration = 3; // 3 detik per gambar di fase mengingat
-  int _quizDuration = 30; // 30 detik per set kuis
-  int _remainingTime = 0; // waktu tersisa
+  int _waktuIngat = 3;
+  int _waktuKuis = 30;
+  int _detik = 0;
 
   bool _quizStarted = false; // apakah kuis sudah dimulai
   bool _memorizationPhase = true; // apakah fase mengingat masih berlangsung
 
   int _memorizationIndex = 0; // indeks gambar di fase mengingat
-  List<String> _memorizedImages = []; // gambar-gambar yang diingat
-  List<List<String>> _quizSets = []; // set kuis dengan 4 pilihan
+  List<String> _gambarDiingat = []; // gambar-gambar yang diingat
+  List<List<String>> _gambarKuis = []; // set kuis dengan 4 pilihan
   int _currentSet = 0; // set kuis saat ini
   int _score = 0; // skor pemain
 
   @override
   void initState() {
     super.initState();
-    _memorizedImages = generateMemorizationImages(
-        5); // menghasilkan 5 gambar untuk fase mengingat
-    _remainingTime = _memorizationDuration; // waktu awal untuk fase mengingat
-    startMemorization(); // mulai fase mengingat
+    _gambarDiingat =
+        buatGambarUntukDiingat(5); // menghasilkan 5 gambar untuk fase mengingat
+    _detik = _waktuIngat; // waktu awal untuk fase mengingat
+    mulaiMengingat(); // mulai fase mengingat
   }
 
-  void startMemorization() {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+  void mulaiMengingat() {
+    _timer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {
       setState(() {
-        _remainingTime--; // kurangi waktu
-        if (_remainingTime <= 0) {
-          if (_memorizationIndex < _memorizedImages.length - 1) {
+        _detik--; // kurangi waktu
+        if (_detik <= 0) {
+          if (_memorizationIndex < _gambarDiingat.length - 1) {
             _memorizationIndex++; // pindah ke gambar berikutnya
-            _remainingTime =
-                _memorizationDuration; // reset waktu untuk gambar berikutnya
+            _detik = _waktuIngat; // reset waktu untuk gambar berikutnya
           } else {
             _memorizationPhase = false; // fase mengingat selesai
             _quizStarted = true; // fase kuis dimulai
-            _quizSets = generateQuizSets(); // buat set kuis
+            _gambarKuis = generateQuizSets(); // buat set kuis
             _currentSet = 0; // set kuis pertama
-            _remainingTime = _quizDuration; // reset waktu untuk kuis
+            _detik = _waktuKuis; // reset waktu untuk kuis
             _timer.cancel(); // hentikan timer fase mengingat
             startQuiz(); // mulai fase kuis
           }
@@ -57,14 +57,13 @@ class _GameState extends State<Game> {
   }
 
   void startQuiz() {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+    _timer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {
       setState(() {
-        _remainingTime--; // kurangi waktu
-        if (_remainingTime <= 0) {
-          if (_currentSet < _quizSets.length - 1) {
+        _detik--; // kurangi waktu
+        if (_detik <= 0) {
+          if (_currentSet < _gambarKuis.length - 1) {
             _currentSet++; // pindah ke set berikutnya
-            _remainingTime =
-                _quizDuration; // reset waktu untuk set kuis berikutnya
+            _detik = _waktuKuis; // reset waktu untuk set kuis berikutnya
           } else {
             _timer.cancel(); // hentikan timer
             // Logika akhir game atau fase kuis selesai
@@ -74,25 +73,32 @@ class _GameState extends State<Game> {
     });
   }
 
-  List<String> generateMemorizationImages(int count) {
+  List<String> buatGambarUntukDiingat(int count) {
     Random random = Random();
-    Set<String> images = Set();
+    Set<int> uniqueSets = {}; // menyimpan set unik
+    List<String> images = [];
 
-    while (images.length < count) {
+    while (uniqueSets.length < count) {
       int setNumber = 1 + random.nextInt(20); // set acak 1-20
-      int imageNumber = 1 + random.nextInt(4); // gambar dalam set 1-4
-      String imageName = "c-$setNumber-$imageNumber"; // format gambar
-      images.add(imageName);
+      int imageNumber = 1 + random.nextInt(4); // gambar acak 1-4
+
+      if (uniqueSets.contains(setNumber)) {
+        continue; // jika set sudah ada, lanjutkan loop
+      }
+
+      String imageName = "c-$setNumber-$imageNumber"; // format nama gambar
+      images.add(imageName); // tambahkan gambar
+      uniqueSets.add(setNumber); // tambahkan set unik
     }
 
-    return images.toList(); // kembalikan daftar gambar
+    return images; // kembalikan daftar gambar
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("MemorImage"),
+        title: const Text("MemorImage"),
       ),
       body: Center(
         child: _memorizationPhase
@@ -103,60 +109,110 @@ class _GameState extends State<Game> {
   }
 
   Widget memorizationUI() {
-    return Column(
-      children: [
-        Text("Ingat gambar-gambar ini",
-            style: TextStyle(fontSize: 20)), // teks judul
-        if (_memorizationIndex < _memorizedImages.length)
-          Image.asset(
-              "assets/images/${_memorizedImages[_memorizationIndex]}.png"), // gambar fase mengingat
-        Text("Waktu tersisa: ${formatTime(_remainingTime)}",
-            style: TextStyle(fontSize: 20)), // waktu tersisa
-      ],
-    );
+    return Center(
+        child: SingleChildScrollView(
+      child: Column(
+        children: [
+          const Text("Ingat gambar-gambar ini", style: TextStyle(fontSize: 20)),
+          LinearPercentIndicator(
+            center: Text(
+              formatTime(_detik),
+              style: TextStyle(fontSize: 16),
+            ),
+            width: MediaQuery.of(context).size.width,
+            lineHeight: 20.0,
+            percent: (_detik / _waktuIngat),
+            backgroundColor: Colors.grey,
+            progressColor: Colors.red,
+          ),
+          // teks judul
+          if (_memorizationIndex < _gambarDiingat.length)
+            Image.asset(
+                "assets/images/${_gambarDiingat[_memorizationIndex]}.png"), // gambar fase mengingat // waktu tersisa
+        ],
+      ),
+    ));
   }
 
   Widget quizUI() {
-    if (_currentSet >= _quizSets.length) {
-      return Column(
+    if (_currentSet >= _gambarKuis.length) {
+      return Center(
+          child: SingleChildScrollView(
+              child: Column(
         children: [
           Text("Game selesai! Skor Anda: $_score",
-              style: TextStyle(fontSize: 20)), // teks akhir
+              style: const TextStyle(fontSize: 20)), // teks akhir
           ElevatedButton(
             onPressed: () =>
                 Navigator.pop(context), // kembali ke layar sebelumnya
-            child: Text("Kembali"),
+            child: const Text("Kembali"),
           ),
         ],
-      );
+      )));
     }
 
-    List<String> currentChoices = _quizSets[_currentSet]; // set kuis saat ini
+    List<String> currentChoices = _gambarKuis[_currentSet]; // set kuis saat ini
 
-    return Column(
-      children: [
-        Text("Pilih gambar yang benar",
-            style: TextStyle(fontSize: 20)), // teks judul fase kuis
-        Text("Waktu tersisa: ${formatTime(_remainingTime)}",
-            style: TextStyle(fontSize: 20)), // teks waktu tersisa
-        Row(
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
           children: [
-            for (int i = 0; i < 4; i++) // 4 pilihan gambar
-              ElevatedButton(
-                onPressed: () => checkAnswer(currentChoices[i]), // cek jawaban
-                child: Image.asset(
-                    "assets/images/${currentChoices[i]}.png"), // pilihan gambar
-              ),
+            const Text("Pilih gambar yang benar",
+                style: TextStyle(fontSize: 20)), // teks judul fase kuis
+            CircularPercentIndicator(
+              radius: 90.0,
+              lineWidth: 20.0,
+              percent: 1 - (_detik / _waktuKuis),
+              center: Text(formatTime(_detik)),
+              progressColor: Colors.red,
+            ),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    OutlinedButton(
+                      onPressed: () =>
+                          cekJawaban(currentChoices[0]), // cek jawaban
+                      child: Image.asset(
+                          "assets/images/${currentChoices[0]}.png"), // pilihan gambar
+                    ),
+                    OutlinedButton(
+                      onPressed: () =>
+                          cekJawaban(currentChoices[1]), // cek jawaban
+                      child: Image.asset(
+                          "assets/images/${currentChoices[1]}.png"), // pilihan gambar
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    OutlinedButton(
+                      onPressed: () =>
+                          cekJawaban(currentChoices[2]), // cek jawaban
+                      child: Image.asset(
+                          "assets/images/${currentChoices[2]}.png"), // pilihan gambar
+                    ),
+                    OutlinedButton(
+                      onPressed: () =>
+                          cekJawaban(currentChoices[3]), // cek jawaban
+                      child: Image.asset(
+                          "assets/images/${currentChoices[3]}.png"), // pilihan gambar
+                    ),
+                  ],
+                )
+              ],
+            ),
           ],
         ),
-        Text("Skor: $_score", style: TextStyle(fontSize: 20)), // teks skor
-      ],
+      ),
     );
   }
 
-  void checkAnswer(String selectedOption) {
+  void cekJawaban(String selectedOption) {
     // Jawaban yang benar adalah gambar yang sesuai dengan set saat ini
-    String correctAnswer = _memorizedImages[_currentSet];
+    String correctAnswer = _gambarDiingat[_currentSet];
 
     // Jika jawaban yang dipilih adalah jawaban yang benar, tingkatkan skor
     if (selectedOption == correctAnswer) {
@@ -164,9 +220,9 @@ class _GameState extends State<Game> {
     }
 
     // Setelah memeriksa jawaban, pindah ke set kuis berikutnya
-    if (_currentSet < _quizSets.length - 1) {
+    if (_currentSet < _gambarKuis.length - 1) {
       _currentSet++; // pindah ke set berikutnya
-      _remainingTime = _quizDuration; // reset waktu untuk set kuis berikutnya
+      _detik = _waktuKuis; // reset waktu untuk set kuis berikutnya
     } else {
       _timer.cancel(); // hentikan timer
       // Logika untuk akhir kuis (misalnya, menampilkan hasil akhir)
@@ -179,7 +235,7 @@ class _GameState extends State<Game> {
     List<List<String>> quizSets = [];
     Random random = Random();
 
-    for (var memorizedImage in _memorizedImages) {
+    for (var memorizedImage in _gambarDiingat) {
       List<String> quizOptions = [];
       int correctSet = int.parse(memorizedImage
           .split('-')[1]); // ekstrak nomor set dari gambar yang diingat
